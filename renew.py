74a05ff606bfaec6b71fa3f39ac2b48024b77897
TransportLayer <TransportLayerOTR@gmail.com>
2017-08-15 03:40:49 -0700

    Add cwd argument to run

--- renew.py
+++ renew.py
@@ -16,10 +16,8 @@ except CalledProcessError:
 for site in listdir(LETSENCRYPT_LIVE):
     try:
         site_dir = "{}/{}".format(LETSENCRYPT_LIVE, site)
-        print("Changing to directory {}".format(site_dir))
-        run("cd {}".format(site_dir), shell=True, check=True)
-        print("Creating combined pem file")
-        run("cat fullchain.pem privkey.pem > {}/{}.pem".format(HAPROXY_CERTS, site), shell=True, check=True)
+        print("Creating combined pem file for {}".format(site))
+        run("cat fullchain.pem privkey.pem > {}/{}.pem".format(HAPROXY_CERTS, site), cwd=site_dir, shell=True, check=True)
     except CalledProcessError:
         print("Error combining pem file for site {}".format(site))
 
